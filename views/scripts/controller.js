a94ba2eb2c7d04bc56a0e148ef06db8f8a5b4c81
unknown <mukesh.nmiet@gmail.com>
2015-10-18 19:08:56 +0530

    Add details to my file

---
+++ views/scripts/controller.js
@@ -0,0 +1,20 @@
+function getData(){
+                $(document).ready(
+                    function(){
+                          $("#userFNameValue").load("http://localhost/infobox/model/database.php", function(responseTxt, statusTxt, xhr){
+        // $("#userFNameValue").load(xhr.setRequestHeader);          
+        if(statusTxt == "success"){
+            data = JSON.parse(responseTxt); /// Parsing the object
+            allRecords = data.record;
+        
+            for(x in allRecords){                
+                //alert(allRecords[x].roll);
+               $("#serverdata").append("<Div>" + allRecords[x].name + "  " + allRecords[x].roll + " " + "</Div>");
+            }
+        }
+        if(statusTxt == "error")
+            alert("Error: " + xhr.status + ": " + xhr.statusText);
+    });   
+                    }
+                );
+            }
